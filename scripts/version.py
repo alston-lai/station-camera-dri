db2b4a917942fba0ef8befd66c48ea4abb47bc4a
Alston Lai <Alston.lai@luxshare-ict.com>
2026-09-12 08:03:11 +0800

    -update version

--- scripts/version.py
+++ scripts/version.py
@@ -1,7 +1,7 @@
 # version.py
-__version__ = "1.2.3"
+__version__ = "2.2.3"
 __version_info__ = {
-    "major": 1,
+    "major": 2,
     "minor": 2,
     "patch": 3,
     "release_date": "2026-09-10",
